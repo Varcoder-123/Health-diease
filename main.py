e63715fd6ceb79e4c18a2c09dcbf54c8563bf444
Vignesh V <94730740+Varcoder-123@users.noreply.github.com>
2024-04-28 12:47:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
-# training imports
+# training imports.
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
